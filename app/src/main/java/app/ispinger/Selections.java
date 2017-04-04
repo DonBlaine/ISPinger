4b837621e8b2f5174af4bad2efa63e8f362d6624
DonBlaine <donovanjblaine@gmail.com>
2017-04-04 02:25:41 -0700

    Initial commit

---
+++ app/src/main/java/app/ispinger/Selections.java
@@ -0,0 +1,13 @@
+package app.ispinger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Selections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selections);
+    }
+}
